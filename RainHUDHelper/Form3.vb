ac2d7e0d1703697cf6f4dbf41fc525757726f747
SentimentalWoosh <13785032+SentimentalWoosh@users.noreply.github.com>
2019-03-15 18:36:38 +0100

    Moved old version to legacy branch

--- RainHUDHelper/Form3.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form3
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        My.Settings.cPath = PathTextBoxIntro.Text
-        Form1.Show()
-        Me.Hide()
-    End Sub
-End Class
